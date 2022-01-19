30c9ea183f39ff0c37a8da2fc5bf8af8b1cc754f
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2022-01-19 16:56:14 +0100

    Enable using SSM parameters

--- lib/secret.ts
+++ lib/secret.ts
@@ -1,6 +1,7 @@
 import * as iam from "aws-cdk-lib/aws-iam";
 import { ISecret } from "aws-cdk-lib/aws-secretsmanager";
 import { IResolvable, IResolveContext, SecretValue } from "aws-cdk-lib";
+import { IStringParameter } from "aws-cdk-lib/aws-ssm";
 
 const SqlSecretSymbol = Symbol("SqlSecret")
 
@@ -25,6 +26,13 @@ export abstract class SqlSecret implements IResolvable {
   static fromSecretsManager(secret: ISecret, field?: string): SqlSecret {
     return new class extends SqlSecret {
       readonly value: SecretValue = field ? secret.secretValueFromJson(field) : secret.secretValue;
+      grantRead = (grantee: iam.IGrantable) => secret.grantRead(grantee)
+    }
+  }
+
+  static fromSSMParameter(secret: IStringParameter, version?: string): SqlSecret {
+    return new class extends SqlSecret {
+      readonly value: SecretValue = SecretValue.ssmSecure(secret.parameterName, version)
       grantRead = (grantee: iam.IGrantable) => secret.grantRead(grantee)
     }
   }
